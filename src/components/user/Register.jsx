0cc279f2559db67e62bbf0602bf9ea7258fe46c5
rrr <meesalaramunaidu1@gmail.com>
2025-06-20 15:30:18 +0530

    Registartion from validation done

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -3,30 +3,63 @@ import { CgNametag } from "react-icons/cg";
 import { FaUserNurse } from "react-icons/fa6";
 import { TbPassword } from "react-icons/tb";
 import { FaRepeat } from "react-icons/fa6";
+import {validatePassword} from "val-pass"
+import toast from 'react-hot-toast';
+
 
 
 const Register = () => {
-let [state,setState]=useState({})
-let[repeatpas,setPassword]=useState()
-
-let handlePassword =()=>{
+let [state,setState]=useState({
+  name:"",
+  username:"", 
+  email:"",
+  password:""
+})
+let[match,setMatched]=useState(true)
+let [errormessage,setErrormessage]=useState("")
+  
+let handlePassword =(e)=>{
+  let {value}=e.target
+  state.password!=value?setMatched(false):setMatched(true)
+value=="" && setMatched(true)
   
 }
 
 let handleChange=(e)=>{
   let{name,value}=e.target
   setState((preval)=>({...preval,[name]:value})) 
+  if(name=="password")
+  {
+    let {validateAll,getAllValidationErrorMessage}=validatePassword(value,8)
+    validateAll()?setErrormessage(""):setErrormessage(getAllValidationErrorMessage())
+    value=="" && setErrormessage("")
+  }
 }
 let handleSumbit=(e)=>{
   e.preventDefault()
-  console.log(state);
+ 
+  let {name,username,password,email}=state
+    if(!name||!username||!password||!email){
+      toast.error("All feilds are mandatory")
+      return
+    }
+
+   let {validateAll,getAllValidationErrorMessage}=validatePassword(password)
+  if(!validateAll()){
+      toast.error(`${getAllValidationErrorMessage()}`)
+    }
+    if(!match){
+      toast.error("passsword and confirm password did not match")
+  return
+    }
+console.log(state);
   
 }
 
 
   return (
-    <div  className='  bg-[#bfb4b46f] bg-[url()] size-full flex justify-center items-center'  >
-        <form action="" onSubmit={handleSumbit} className='bg-white h-[90%] max-sm:w-[90%]  rounded-bl-lg shadow-2xl w-[35%] flex justify-center items-center flex-col gap-6 py-10  ' >
+    <div  className='  bg-[#bfb4b46f] bg-[url()] size-full  flex justify-center items-center'  >
+        <form action="" onSubmit={handleSumbit} className='bg-white h-[90%] max-sm:w-[90%]  overflow-scroll rounded-bl-lg shadow-2xl w-[35%] flex  items-center flex-col gap-6 py-14  ' >
 
           <div className='w-[80%]  py-3 bg-white flex justify-center items-center rounded-sm'>
             <h1  className='text-[28px] max-sm:text-sm '>Registration form</h1>
@@ -49,22 +82,28 @@ let handleSumbit=(e)=>{
             <span><CgNametag /></span>
           </div>      
 
+
+
            <div  className='w-[80%] bg-white flex justify-center items-center border-1  rounded-sm   px-5  py-3' >
             <input type="password" name="password" placeholder='Enter your password'  className='  w-[100%] outline-0 text-[18px]'onChange={handleChange} />
             <span><CgNametag /></span>
           </div>
 
-           <div  className='w-[80%] bg-white flex justify-center items-center border-1  rounded-sm   px-5  py-3'>
-            <input type="password" name="repeatpassword" placeholder=' Repeat password'   className='  w-[100%] outline-0 text-[18px]' onClick={handleChange}/>
+          <div className={ errormessage?'w-[80%] bg-white flex justify-center items-center text-red-400  rounded-sm   px-5  py-3  ':'hidden'}>
+            <span>{errormessage}</span>
+          </div>
+ 
+           <div  className={`w-[80%] bg-white flex justify-center items-center border-1  rounded-sm   px-5  py-3 ${!match?'border-red-600':'border-l-black'}`}>
+            <input type="password" name="repeatpassword" placeholder=' Repeat password'   className='  w-[100%] outline-0 text-[18px]' onChange={handlePassword}/>
             <span><FaRepeat /></span>
           </div>
 
-          <div className='w-[80%] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ... flex justify-center items-center border-0 rounded-sm  tracking-[0.5px] text-[19px] py-3'>
-            <button>click</button>
+          <div className='w-[80%] bg-amber-200 flex justify-center items-center border-0 rounded-sm  tracking-[0.5px] text-[19px] py-3 hover:bg-amber-400 '>
+            <button className='w-full'>click</button>
             </div>
         </form>
     </div>
   )
 }
 
-export default Register  
+export default Register   
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,8 @@
 import  { createRoot } from "react-dom/client"
 import App from "./App"
+import { Toaster } from "react-hot-toast"
 
-createRoot(document.getElementById("root")).render(<App></App>)
+createRoot(document.getElementById("root")).render(<>
+<App></App>
+<Toaster></Toaster>
+</>)
