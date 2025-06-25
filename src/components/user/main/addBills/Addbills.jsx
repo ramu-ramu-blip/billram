120e1229b44ba9e20674ce623737b631a99b07d6
rrr <meesalaramunaidu1@gmail.com>
2025-06-25 14:00:00 +0530

    addbill page created

--- src/components/user/main/addBills/Addbills.jsx
+++ src/components/user/main/addBills/Addbills.jsx
@@ -1,8 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Addbills = () => {
+
+  const [bill,setBill]=useState(
+    {
+    companyName:"",
+    PoNo:"",
+    invoiceDate:"",
+    workCompletionDate:"",
+    address:"",
+    PAN:"",
+    GSTNo:"",
+    clientBankName:""
+    }
+  )
+
+  const handleChange =(e)=>{
+    let{name,value}=e.target
+    setBill((preVal)=>({...preVal,[name]:value}))
+  }
+
+  const handleSumbit=(e)=>{
+    e.preventDefault()
+ console.log(bill);
+ 
+  }
+
   return (
-    <div>Addbills</div>
+     <div  className='  bg-[#bfb4b46f] bg-[url()] size-full  flex justify-center items-center'  >
+            <form action="" onSubmit={handleSumbit} className='bg-white h-[90%] max-sm:w-[90%]  overflow-scroll rounded-bl-lg shadow-2xl w-[35%] flex  items-center flex-col gap-6 py-14  ' >
+    
+              <div className='w-[80%]  py-3 bg-white flex justify-center items-center rounded-sm'>
+                <h1  className='text-[28px] max-sm:text-sm '>Add Bill form</h1>
+              </div>
+    
+              <div  className='w-[80%] bg-white flex justify-center items-center border-1  rounded-sm   px-5  py-3'>
+                <input  type="text" name="companyName" placeholder='Enter your companyName' className='  w-[100%] outline-0 text-[18px]' onChange={handleChange} />
+               
+              </div>
+    
+               <div className='w-[80%] bg-white flex justify-center items-center border-1  rounded-sm   px-5  py-3'>
+                <input type="text" name="PoNo" placeholder='Enter your PoNo'   className='  w-[100%] outline-0 text-[18px]' onChange={handleChange}/>
+              
+              </div>
+
+              
+               <div className='w-[80%] bg-white flex justify-center items-center border-1  rounded-sm   px-5  py-3'>
+                <input type="date" name="workCompletionDate" placeholder='Enter your work Completion Date'   className='  w-[100%] outline-0 text-[18px]' onChange={handleChange}/>
+                
+              </div>
+
+              
+               <div className='w-[80%] bg-white flex justify-center items-center border-1  rounded-sm   px-5  py-3'>
+                <input type="text" name="address" placeholder='Enter your address'   className='  w-[100%] outline-0 text-[18px]' onChange={handleChange}/>
+               
+              </div>
+              
+               <div className='w-[80%] bg-white flex justify-center items-center border-1  rounded-sm   px-5  py-3'>
+                <input type="text" name="PAN" placeholder='Enter your PAN'   className='  w-[100%] outline-0 text-[18px]' onChange={handleChange}/>
+              
+              </div>
+
+              <div className='w-[80%] bg-white flex justify-center items-center border-1  rounded-sm   px-5  py-3'>
+                <input type="text" name="GSTNo" placeholder='Enter your GSTNo'   className='  w-[100%] outline-0 text-[18px]' onChange={handleChange}/>
+                
+              </div>
+
+              <div className='w-[80%] bg-white flex justify-center items-center border-1  rounded-sm   px-5  py-3'>
+                <input type="text" name="clientBankName" placeholder='Enter your clientBankName'   className='  w-[100%] outline-0 text-[18px]' onChange={handleChange}/>
+               
+              </div>
+
+    
+              
+    
+              <div className='w-[80%] bg-amber-200 flex justify-center items-center border-0 rounded-sm  tracking-[0.5px] text-[19px] py-3 hover:bg-amber-400  active:bg-green-300 active:scale-[0.9]'>
+                <button className='w-full'>click</button>
+                </div>
+            </form>
+        </div>
   )
 }
 
--- src/components/user/main/navbar/Navbar.jsx
+++ src/components/user/main/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router'
+import { Link, NavLink } from 'react-router'
 import { TfiMenuAlt } from "react-icons/tfi";
 import { IoIosCloseCircle } from "react-icons/io";
 
@@ -15,29 +15,31 @@ const Navbar = () => {
   
   return (
     
-   <div className={` '   bg-amber-300 w-full h-[80px] flex justify-around items-center text-xl ${show?'flex flex-col justify-around gap-2 items-start px-7 py-6 h-[250px]':""} sm:flex-row sm:h-[80px] sm:justify-around sm:items-center  `}>
+   <div className={` '    bg-amber-300 w-full h-[80px] flex justify-around items-center
+     text-xl ${show?'flex flex-col justify-around gap-2 items-start px-7 py-6 h-[250px]':""} 
+     sm:flex-row sm:h-[80px] sm:justify-around sm:items-center  `}>
          
 
-        <div className={`hidden  max-sm:flex  `}> 
+        <div className={`hidden max-sm:flex  `}> 
          {
            show ? <IoIosCloseCircle onClick={hadleShow}  className='text-4xl justify-start items-start'/>: <TfiMenuAlt onClick={hadleShow}/>
          }
         </div>
 
          <div   className={`max-sm:hidden${show?'block':""}`}>
-          <Link to="/home">Home</Link>
+          <NavLink    className={({isActive})=>isActive?"bg-amber-100 p-2 rounded-sm ":""} end  to="/home">Home</NavLink>
           </div>
-
-        <div    className={`max-sm:hidden${show?'block':""}`}>
-          <Link to="addBills" >AddBills</Link>
+ 
+        <div    className={`max-sm:hidden${show?'block':""}`}>   
+          <NavLink  className={({isActive})=>isActive?"bg-amber-100 p-2 rounded-sm ":""}  to="addBills" >AddBills</NavLink>
           </div>
 
         <div   className={`max-sm:hidden${show?'block':""}`}>
-          <Link to="filterBills">FilterBills</Link>
+          <NavLink className={({isActive})=>isActive?"bg-amber-100 p-2 rounded-sm ":""}  to="filterBills">FilterBills</NavLink>
         </div>
 
         <div    className={`max-sm:hidden${show?'block':""}`}>
-          <Link to="about">About</Link>
+          <NavLink className={({isActive})=>isActive?"bg-amber-100 p-2 rounded-sm ":""}   to="about">About</NavLink>
         </div>
                
        
